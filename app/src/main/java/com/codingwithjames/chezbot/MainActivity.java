36c9cd2b324290c152b90d7f30cfb3fe56e4446d
jamesfdz <jfernandes942@gmail.com>
2019-05-25 17:14:51 +0530

    v1

--- app/src/main/java/com/codingwithjames/chezbot/MainActivity.java
+++ app/src/main/java/com/codingwithjames/chezbot/MainActivity.java
@@ -2,8 +2,17 @@ package com.codingwithjames.chezbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private FloatingActionButton mButtonSend;
+    private EditText mEditTextMessage;
+    private ImageView mImageView;
+    private ChatMessageAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
